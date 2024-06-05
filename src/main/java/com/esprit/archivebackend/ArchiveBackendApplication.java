fda2479e06802d6c89e0e7acf5f8397ea16601fe
SnapeV1 <Hamada_amine@yahoo.com>
2024-06-05 20:34:04 +0100

    Project creation

---
+++ src/main/java/com/esprit/archivebackend/ArchiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.archivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiveBackendApplication.class, args);
+    }
+
+}
